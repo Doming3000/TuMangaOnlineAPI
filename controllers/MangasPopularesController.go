53e1b7563242ff81b16051beaa024a35b278c76d
Doming3000 <82414336+Doming3000@users.noreply.github.com>
2025-04-26 15:18:57 -0400

    Update MangasPopularesController.go

--- controllers/MangasPopularesController.go
+++ controllers/MangasPopularesController.go
@@ -4,17 +4,18 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/julioolivares90/TumangaOnlineApi/core/tumangaonline"
 	"github.com/julioolivares90/TumangaOnlineApi/models"
 )
 
-///GetMangasPopularesWithPagination
 func GetMangasPopularesWithPagination(c *fiber.Ctx) error {
 
 	param := c.Query("pageNumber", "0")
-	fmt.Println(param)
+	log.Println("Página solicitada:", param)
+
 	id, err := strconv.Atoi(param)
 	if err != nil {
 		response := models.Response{
@@ -23,27 +24,28 @@ func GetMangasPopularesWithPagination(c *fiber.Ctx) error {
 		}
 		return c.JSON(response)
 	}
+
+	var mangasPopulares []models.Manga
 	if id > 0 {
-		mangasPopulares := tumangaonline.GetMangasPopulares(id)
+		log.Printf("Obteniendo mangas populares de la página %d", id)
+		mangasPopulares = tumangaonline.GetMangasPopulares(id)
+	} else {
+		log.Println("Página no válida o 0, obteniendo mangas populares de la página 1")
+		mangasPopulares = tumangaonline.GetMangasPopulares(1)
+	}
+
+	if len(mangasPopulares) == 0 {
+		log.Println("No se encontraron mangas populares.")
 		response := models.Response{
-			StatusCode: http.StatusOK,
-			Data:       mangasPopulares,
-		}
-		return c.JSON(response)
-	} else if id < 0 {
-		mangasPopulares := tumangaonline.GetMangasPopulares(1)
-		response := models.Response{
-			StatusCode: http.StatusOK,
-			Data:       mangasPopulares,
-		}
-		return c.JSON(response)
-	} else if id == 0 {
-		mangasPopulares := tumangaonline.GetMangasPopulares(1)
-		response := models.Response{
-			StatusCode: http.StatusOK,
-			Data:       mangasPopulares,
+			StatusCode: http.StatusNotFound,
+			Data:       "No se encontraron mangas populares en esta página.",
 		}
 		return c.JSON(response)
 	}
-	return c.JSON("")
+
+	response := models.Response{
+		StatusCode: http.StatusOK,
+		Data:       mangasPopulares,
+	}
+	return c.JSON(response)
 }
