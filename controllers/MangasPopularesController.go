3ee8a564659c3801aacc8062547ef3ab65e35c5e
Doming3000 <82414336+Doming3000@users.noreply.github.com>
2025-04-26 15:26:29 -0400

    Update MangasPopularesController.go

--- controllers/MangasPopularesController.go
+++ controllers/MangasPopularesController.go
@@ -4,18 +4,17 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/julioolivares90/TumangaOnlineApi/core/tumangaonline"
 	"github.com/julioolivares90/TumangaOnlineApi/models"
 )
 
+///GetMangasPopularesWithPagination
 func GetMangasPopularesWithPagination(c *fiber.Ctx) error {
 
 	param := c.Query("pageNumber", "0")
-	log.Println("Página solicitada:", param)
-
+	fmt.Println(param)
 	id, err := strconv.Atoi(param)
 	if err != nil {
 		response := models.Response{
@@ -24,28 +23,27 @@ func GetMangasPopularesWithPagination(c *fiber.Ctx) error {
 		}
 		return c.JSON(response)
 	}
-
-	var mangasPopulares []models.Manga
 	if id > 0 {
-		log.Printf("Obteniendo mangas populares de la página %d", id)
-		mangasPopulares = tumangaonline.GetMangasPopulares(id)
-	} else {
-		log.Println("Página no válida o 0, obteniendo mangas populares de la página 1")
-		mangasPopulares = tumangaonline.GetMangasPopulares(1)
-	}
-
-	if len(mangasPopulares) == 0 {
-		log.Println("No se encontraron mangas populares.")
+		mangasPopulares := tumangaonline.GetMangasPopulares(id)
 		response := models.Response{
-			StatusCode: http.StatusNotFound,
-			Data:       "No se encontraron mangas populares en esta página.",
+			StatusCode: http.StatusOK,
+			Data:       mangasPopulares,
+		}
+		return c.JSON(response)
+	} else if id < 0 {
+		mangasPopulares := tumangaonline.GetMangasPopulares(1)
+		response := models.Response{
+			StatusCode: http.StatusOK,
+			Data:       mangasPopulares,
+		}
+		return c.JSON(response)
+	} else if id == 0 {
+		mangasPopulares := tumangaonline.GetMangasPopulares(1)
+		response := models.Response{
+			StatusCode: http.StatusOK,
+			Data:       mangasPopulares,
 		}
 		return c.JSON(response)
 	}
-
-	response := models.Response{
-		StatusCode: http.StatusOK,
-		Data:       mangasPopulares,
-	}
-	return c.JSON(response)
+	return c.JSON("")
 }
